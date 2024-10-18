2df8916a32e3b46b7ee0bbac446cbad7f38c8df3
Asma <saiyedas@shernet.sheridancollege.ca>
2024-10-18 12:19:17 -0400

    Resolved merge conflicts

--- src/main/java/in/sp/main/beans/Student.java
+++ src/main/java/in/sp/main/beans/Student.java
@@ -1,31 +1,26 @@
 package in.sp.main.beans;
 
-<<<<<<< HEAD
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Student
-{
+public class Student {
+
 	@Value("Asma")
 	private String name;
-	
+
 	@Value("101")
 	private int rollno;
-	
+
 	@Autowired
-=======
-public class Student
-{
-	private String name;
-	private int rollno;
->>>>>>> b0ad5638330469f9f6d2438ec7da2d0cbc113ed7
-	private Address address;
-	
+	private Address address;  // Autowiring Address class
+
+	// Getters and setters
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
@@ -33,6 +28,7 @@ public class Student
 	public int getRollno() {
 		return rollno;
 	}
+
 	public void setRollno(int rollno) {
 		this.rollno = rollno;
 	}
@@ -40,14 +36,15 @@ public class Student
 	public Address getAddress() {
 		return address;
 	}
+
 	public void setAddress(Address address) {
 		this.address = address;
 	}
-	
-	public void display()
-	{
-		System.out.println("Name : "+name);
-		System.out.println("Rollno : "+rollno);
-		System.out.println("Address : "+address);
+
+	// Display method
+	public void display() {
+		System.out.println("Name : " + name);
+		System.out.println("Rollno : " + rollno);
+		System.out.println("Address : " + address);
 	}
 }
